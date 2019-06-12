4d38cc760975fbeff4adeeb96f0eb905f19aecec
Chino <ronaldcubillo92@gmail.com>
2019-06-12 16:29:22 -0500

    Added mongo to the server.js

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ if (process.env.NODE_ENV === "production") {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://user:cdnTq3Z7QH2ZFjb.mlab.com:37267/heroku_m5dtcmtc");
 
 // Start the API server
 app.listen(process.env.PORT, function() {
