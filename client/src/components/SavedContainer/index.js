323b7f78ef40a018e0fe62451c966aa0f175b4b0
Chino <ronaldcubillo92@gmail.com>
2019-06-13 10:55:49 -0500

    Working on the delete  button, new changes

--- client/src/components/SavedContainer/index.js
+++ client/src/components/SavedContainer/index.js
@@ -57,14 +57,14 @@ class SavedContainer extends Component {
                 <span>
                   {/* <a href={object.previewLink}><span>Preview Link </span></a>| */}
                         <a href={object.infoLink}><span> Info Link </span></a>|
-                         <button>  <DeleteBtn onClick={() => this.deleteBook(book._id)} /></button>
                   {/* <span> Delete</span> */}
 
-                {/* {this.state.books.map(book => {
-                  return (
+                {this.state.books.map(book => {
+
                      <button>  <DeleteBtn onClick={() => this.deleteBook(book._id)} /></button>
-                  );
-                })} */}
+
+                })}
+
                 </span>
               </div>
             </>
