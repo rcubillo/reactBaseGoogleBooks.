274ccd5e627f2905dc15e8f56b4cb27e603c8063
Chino <ronaldcubillo92@gmail.com>
2019-06-13 06:39:37 -0500

    Fixing one of the props

--- client/src/components/SearchContainer/index.js
+++ client/src/components/SearchContainer/index.js
@@ -20,7 +20,7 @@ function SearchContainer(props) {
                 return (<div key={i}>
                     <h1>{volumeInfo.title}</h1>
                     <h2>{volumeInfo.authors}</h2>
-                    <img src={volumeInfo.imageLinks.thumbnail} alt="book" />
+                    {/* <img src={volumeInfo.imageLinks.thumbnail} alt="book" /> */}
                     <p>{volumeInfo.description}</p>
                     <span>
                         {/* <a href={volumeInfo.previewLink}><span>Preview Link </span></a>| */}
