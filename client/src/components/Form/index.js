d91971af6ebf74c9ee0a1c3c7c85ceb5b8d556cf
Chino <ronaldcubillo92@gmail.com>
2019-06-12 19:16:57 -0500

    Changed the mongo url

--- client/src/components/Form/index.js
+++ client/src/components/Form/index.js
@@ -1,17 +1,24 @@
 import React from "react";
+import "./styleForm.css";
+
 
 export function Input(props) {
   return (
+    // <div className="formEverything">
     <div className="form-group">
       <input className="form-control" {...props} />
     </div>
+    // </div>
   );
 }
 
 export function FormBtn(props) {
   return (
+    // <div className="formEverything">
     <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn btn-success">
       {props.children}
     </button>
+    // </div>
   );
 }
+
