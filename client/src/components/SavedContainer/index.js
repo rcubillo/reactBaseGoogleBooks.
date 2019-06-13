65ecd97c3a38509d58bae8b5f4840ea78162b0c3
Chino <ronaldcubillo92@gmail.com>
2019-06-13 12:26:38 -0500

    more

--- client/src/components/SavedContainer/index.js
+++ client/src/components/SavedContainer/index.js
@@ -1,11 +1,9 @@
-import React, { Component } from "react";
-import axios from "axios";
-import API from "../../utils/API"
-import DeleteBtn from "../../components/DeleteBtn/index";
-
+import React, { Component } from 'react';
+import axios from 'axios';
+import API from '../../utils/API';
+import DeleteBtn from '../../components/DeleteBtn/index';
 
 class SavedContainer extends Component {
-
   // state = {
   //   books: [],
   //   title: "",
@@ -26,18 +24,19 @@ class SavedContainer extends Component {
   }
 
   loadBooks = () => {
-    axios.get("/api/books")
+    axios
+      .get('/api/books')
       .then(res => this.setState({ books: res.data }))
       // console.log(res.data[0].title)
       .catch(err => console.log(err));
   };
 
   deleteBook = id => {
-    axios.delete("/api/books/" + id)
+    axios
+      .delete('/api/books/' + id)
       .then(res => this.loadBooks())
       .catch(err => console.log(err));
   };
-
 
   render() {
     //console.log(this.state.books)
@@ -45,33 +44,47 @@ class SavedContainer extends Component {
     //console.log(bookList)
     return (
       <>
-        <span>{bookList.map( object => {
-          console.log(object)
-          return (
-            <>
-              <div key={object._id} data={object}>
-                <h1>{object.title}</h1>
-                <h2>{object.authors}</h2>
-                <img src={object.image} alt="book" />
-                <p >{object.description}</p>
-                <span>
-                  {/* <a href={object.previewLink}><span>Preview Link </span></a>| */}
-                        <a href={object.infoLink}><span> Info Link </span></a>|
-                       <a DeleteBtn onClick={() => this.deleteBook(book._id)}><span> Info Link </span></a>|
-                  {/* <span> Delete</span> */}
-
-                {/* {this.state.books.map(book => {
+        <span>
+          {bookList.map(object => {
+            console.log(object);
+            return (
+              <>
+                <div key={object._id} data={object}>
+                  <h1>{object.title}</h1>
+                  <h2>{object.authors}</h2>
+                  <img src={object.image} alt="book" />
+                  <p>{object.description}</p>
+                  <span>
+                    {/* <a href={object.previewLink}><span>Preview Link </span></a>| */}
+                    <a href={object.infoLink}>
+                      <span> Info Link </span>
+                    </a>
+                    |
+                    <span>
+                      {this.state.books.map(book => {
+                        return (
+                          <button>
+                            {' '}
+                            <DeleteBtn
+                              onClick={() => this.deleteBook(book._id)}
+                            />
+                          </button>
+                        );
+                      })}
+                    </span>
+                    {/* {this.state.books.map(book => {
                   return (
                      <button>  <DeleteBtn onClick={() => this.deleteBook(book._id)} /></button>
                   );
                 })} */}
-                </span>
-              </div>
-            </>
-          )
-        })}</span>
+                  </span>
+                </div>
+              </>
+            );
+          })}
+        </span>
       </>
-    )
+    );
   }
 }
 
